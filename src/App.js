8316863c02c4ccdb33168b8f997735156308dcb8
Your Name <adeyeye17@gmail.com>
2021-09-20 20:50:38 +0200

    React API fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      loading: false,
+      character:{},
+      sex:""
+    }
+  }
+
+
+  componentDidMount(){
+    this.setState({loading:true})
+    console.log("did mount")
+    fetch('https://swapi.dev/api/people/4/')
+    .then(response => response.json())
+    .then(data => this.setState({
+      loading:false,
+      character: data,
+      sex:data
+    }));
+  }
+
+
+
+
+render(){
+  console.log("rendered")
+
+  const info = this.state.loading ? "Loading...........": <div> NAME: {this.state.character.name } <br/> SEX:{this.state.sex.gender }</div>
+  return(
+    <div>
+      <h1>{info}</h1>
+    </div>
+  )
+}}
